120abba6a97c23bb6048f9dcdf70d969f4146a06
Lorenzo Grillo <grillo383@gmail.com>
2023-10-08 16:09:25 +0200

    make pyforest actually to work

--- main.py
+++ main.py
@@ -1,38 +1,59 @@
+from forest import RandomForest
 import joblib
 import pathlib
 
-from pyforest.forest import RandomForest
-
-from sklearn.model_selection import train_test_split
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-
-import cv2
+import pandas as pd
 import numpy as np
+import cv2
 
 
 TRAIN_PATH = "./archive/train/"
 data_train_dir = pathlib.Path(TRAIN_PATH)
 
-imagesPath = []
+imagesData = []
+imagesLabel = []
 
-print("Load dataset from path")
+labelTextToNumber = {
+    "angry": 0,
+    "disgust": 1,
+    "fear": 2,
+    "happy": 3,
+    "neutral": 4,
+    "sad": 5,
+    "surprise": 6
+}
 
-for photo in data_train_dir.glob("*.*"):
-    imagesPath.append(photo)
-    
-train_datagen = ImageDataGenerator(rescale = 1./255)
-train_y = train_datagen.flow_from_directory(TRAIN_PATH,
-                                                 target_size = (48, 48),
-                                                 batch_size = 32,
-                                                 class_mode = 'sparse').classes
+def getImageLabel(path):
+    #from before the last / to the last / (escluso)
+    #esempio: ./archive/train/angry/0.jpg
+    #ritorna angry
+    return path.split("\\")[-2]
 
-print("Start training")
-
-rf = RandomForest()
-rf.fit(imagesPath, train_y)
+if __name__ == "__main__":
+    print("Load dataset from path")
 
 
-print("DUMP")
+    # le immagini devono essere caricate e messe in un array nel seguente modo:
+    # [indexImage, imageData (array reshaped 48*48)]
+    # e deve corrispondere ad un label (ogni immagine ha un label)
 
-joblib.dump(model, "./emotional_face_modelV1_2.joblib")
-joblib.dump(train_y, "./emotional_face_modelV1_1_labels.joblib")
+    for dirs in data_train_dir.glob("*"):
+  
+        for photo in dirs.glob("*.*"):
+            imgPath = str(photo)
+
+            image = cv2.imread(imgPath)
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            image = cv2.resize(image, (48, 48))
+            image = image.reshape(48*48)
+
+            imagesLabel.append(labelTextToNumber[getImageLabel(imgPath)])
+            imagesData.append(image)
+
+    imagesData = np.array(imagesData)
+    imagesLabel = np.array(imagesLabel)
+
+    rf = RandomForest()
+
+    rf.fit(imagesData, imagesLabel)
+
